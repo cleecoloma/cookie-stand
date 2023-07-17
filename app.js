1dcb6cdebfa7c1744072f921a0f039f4d4936b3d
Chester Lee Coloma <cleecoloma@gmail.com>
2023-07-17 14:41:54 -0700

    added for loop for modifying li on HTML

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 'use strict';
 
+// This function returns a number between min and max inclusive.
+function randomNumber(min, max) {
+  return Math.floor(
+    Math.random() * (max - min + 1) + min);
+};
+
 const store1 = {
   name: 'Seattle',
   minCustomersPerHour: 23,
   maxCustomersPerHour: 65,
   avgCookiePerCustomer: 6.3,
+  randomCustomers: randomNumber(this.minCustomersPerHour, this.maxCustomersPerHour)
 };
 
 const store2 = {
@@ -12,6 +19,10 @@ const store2 = {
   minCustomersPerHour: 3,
   maxCustomersPerHour: 24,
   avgCookiePerCustomer: 1.2,
+  randomCustomers: randomNumber(
+    this.minCustomersPerHour,
+    this.maxCustomersPerHour
+  ),
 };
 
 const store3 = {
@@ -19,6 +30,10 @@ const store3 = {
   minCustomersPerHour: 11,
   maxCustomersPerHour: 38,
   avgCookiePerCustomer: 3.7,
+  randomCustomers: randomNumber(
+    this.minCustomersPerHour,
+    this.maxCustomersPerHour
+  ),
 };
 
 const store4 = {
@@ -26,6 +41,10 @@ const store4 = {
   minCustomersPerHour: 20,
   maxCustomersPerHour: 38,
   avgCookiePerCustomer: 2.3,
+  randomCustomers: randomNumber(
+    this.minCustomersPerHour,
+    this.maxCustomersPerHour
+  ),
 };
 
 const store5 = {
@@ -33,30 +52,46 @@ const store5 = {
   minCustomersPerHour: 2,
   maxCustomersPerHour: 16,
   avgCookiePerCustomer: 4.6,
+  randomCustomers: randomNumber(
+    this.minCustomersPerHour,
+    this.maxCustomersPerHour
+  ),
 };
-
-//This function returns a number between min and max inclusive.
-function randomNumber(min, max) {
-  let customerPerHour = Math.floor(Math.random() * (max - min + 1) + min);
-  return customerPerHour;
-}
 
 function calculateSales(storeName) {
   const storeSales = []; //this array will store the sales per hour
   //this for loop will calculate the sales per hours
   for (let i = 0; i < 14; i++) {
-    let perHourSales = randomNumber(
-      storeName.minCustomersPerHour,
-      storeName.maxCustomersPerHour
-    );
+    let perHourSales = storeName.randomCustomers;
     storeSales.push(perHourSales);
-    // console.log(perHourSales);
   }
-  // console.log(storeSales);
-  storeName.sales = storeSales;
+  storeName.sales = storeSales; //add a property to the store with key of sales and value of the storeSales array.
   console.log(storeName);
   return storeSales;
 }
 
 calculateSales(store1);
-// console.log(storeSales);
+calculateSales(store2);
+calculateSales(store3);
+calculateSales(store4);
+
+let unorderedList = document.getElementById('main');
+
+function createSalesPerHour(store) {
+  let ulName = document.createElement('ul');
+  ulName.textContent = store.name;
+  unorderedList.appendChild(ulName);
+  for (let i = 0; i < store.sales.length-8; i++) {
+    let listItem = document.createElement('p');
+    listItem.textContent = i+6 + 'am: ' +
+      store.sales[i] + ' cookies';
+    ulName.appendChild(listItem);
+  }
+  for (let i = 0; i < store.sales.length-5; i++) {
+    let listItem = document.createElement('p');
+    listItem.textContent = i+1 + 'pm: ' + store.sales[i] + ' cookies';
+    ulName.appendChild(listItem);
+  }
+}
+
+createSalesPerHour(store1);
