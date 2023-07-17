2008ddbb168a8f4d8748f2e1283972bc531e91a2
Chester Lee Coloma <cleecoloma@gmail.com>
2023-07-17 11:32:06 -0700

    added object literals for each store

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const seattle = {
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  avgCookiePerCustomer: 6.3
+}
+
+const tokyo = {
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  avgCookiePerCustomer: 6.3,
+};
+
+const dubai = {
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  avgCookiePerCustomer: 6.3,
+};
+
+const paris = {
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  avgCookiePerCustomer: 6.3,
+};
+
+const lima = {
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  avgCookiePerCustomer: 6.3,
+};
