3fc8056b2cbf9e0feecfffdbb6a03391e301863f
Chester Lee Coloma <cleecoloma@gmail.com>
2023-07-21 20:46:45 -0700

    fixed stores where paris was showing twice and added placeholder

--- app-index.js
+++ app-index.js
@@ -8,7 +8,7 @@ const seattleStore = {
   address: "4567 Pine Street, Seattle, WA 98102, United States",
   phone: "+1 (555) 123-4567",
   email: "info@salmoncookieco.com",
-  hours: "6AM to 7PM"
+  hours: "6AM to 7PM everyday"
 };
 
 // Tokyo Store
@@ -19,7 +19,7 @@ const tokyoStore = {
   address: '789 Sakura Avenue, Shibuya, Tokyo 150-5678, Japan',
   phone: '+81 (0) 90-9876-5432',
   email: 'contact@sakurasalmoncookies.jp',
-  hours: '6AM to 7PM',
+  hours: '6AM to 7PM everyday',
 };
 
 // Dubai Store
@@ -30,7 +30,7 @@ const dubaiStore = {
   address: '2345 Oasis Road, Jumeirah, Dubai 54321, United Arab Emirates',
   phone: '+971 (0) 50-6789-0123',
   email: 'hello@desertsalmon.com',
-  hours: '6AM to 7PM',
+  hours: '6AM to 7PM everyday',
 };
 
 // Paris Store
@@ -41,7 +41,7 @@ const parisStore = {
   address: '987 Rue de la Mode, Paris 75002, France',
   phone: '+33 (0) 1-2345-6789',
   email: 'contact@lecookiesaumon.fr',
-  hours: '6AM to 7PM',
+  hours: '6AM to 7PM everyday',
 };
 
 // Lima Store
@@ -52,11 +52,11 @@ const limaStore = {
   address: '3456 Avenida de los Andes, Miraflores, Lima 27, Peru',
   phone: '+51 987 654 321',
   email: 'info@andeansalmondreams.pe',
-  hours: '6AM to 7PM',
+  hours: '6AM to 7PM everyday',
 };
 
 // Array the consists of all stores
-const stores = [parisStore, tokyoStore, dubaiStore, parisStore, limaStore]
+const stores = [seattleStore, tokyoStore, dubaiStore, parisStore, limaStore]
 
 // This GET_CARD variable looks that the html and located where store-card id is located
 const GET_CARD = document.getElementById('store-card');
@@ -77,3 +77,4 @@ function createCard(stores) {
 
 // Invokes createCard function using stores array as an argument
 createCard(stores);
+console.log(stores)
